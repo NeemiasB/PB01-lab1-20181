64bc62781630e6357001f3cd33dfcb916c778240
Neemias Barbosa <neemiasbfernandess@hotmail.com>
2018-04-17 12:20:25 -0400

    Add project files.

---
+++ WebAppPB01Lab1_NEEMIAS/Controllers/ClienteController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebAppPB01Lab1_NEEMIAS.Models;
+
+namespace WebAppPB01Lab1_NEEMIAS.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly ClienteContext _context;
+
+        public ClienteController(ClienteContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cliente
+        [Route("")]
+        [Route("cliente")]
+        [Route("cliente/listar")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Cliente.ToListAsync());
+        }
+
+        // GET: Cliente/Details/5
+        [Route("cliente/detalhes/{id:int}")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Cliente
+                .SingleOrDefaultAsync(m => m.ClienteId == id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return View(cliente);
+        }
+
+        // GET: Cliente/Create
+        [Route("cliente/cadastrar")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cliente/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("cliente/cadastrar")]
+        public async Task<IActionResult> Create([Bind("ClienteId,Nome,CPF,Email,DataCadastro")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cliente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);
+        }
+
+        // GET: Cliente/Edit/5
+        [Route("cliente/atualizar/{id:int}")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Cliente.SingleOrDefaultAsync(m => m.ClienteId == id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            return View(cliente);
+        }
+
+        // POST: Cliente/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("cliente/atualizar/{id:int}")]
+        public async Task<IActionResult> Edit(int id, [Bind("ClienteId,Nome,CPF,Email,DataCadastro")] Cliente cliente)
+        {
+            if (id != cliente.ClienteId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cliente);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClienteExists(cliente.ClienteId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);
+        }
+
+        // GET: Cliente/Delete/5
+        [Route("cliente/remover/{id:int}")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Cliente
+                .SingleOrDefaultAsync(m => m.ClienteId == id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return View(cliente);
+        }
+
+        // POST: Cliente/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Route("cliente/remover/{id:int}")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cliente = await _context.Cliente.SingleOrDefaultAsync(m => m.ClienteId == id);
+            _context.Cliente.Remove(cliente);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClienteExists(int id)
+        {
+            return _context.Cliente.Any(e => e.ClienteId == id);
+        }
+    }
+}
---
+++ WebAppPB01Lab1_NEEMIAS/Models/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppPB01Lab1_NEEMIAS.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+
+        [Required(ErrorMessage ="Nome é obrigatorio")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage ="CPF é obrigatorio")]
+        public string CPF { get; set; }
+
+        [Required(ErrorMessage ="Email é obrigatorio")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Data Cadastro é obrigatoria")]
+        public DateTime DataCadastro { get; set; }
+    }
+}
